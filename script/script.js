9c2dd8bbd6fa8ca35260b668333759c4781e9592
Mattis <mattis@petroll.de>
2022-04-27 11:34:28 +0200

    a

--- script/script.js
+++ script/script.js
@@ -1,6 +1,13 @@
 document.getElementById("loop-button").addEventListener("click", clickPlay);
-document.getElementsByClassName("drum").addEventListener("click", clickDrum);
-
+document.getElementById("drum1").addEventListener("click", clickDrum1);
+document.getElementById("drum2").addEventListener("click", clickDrum2);
+document.getElementById("drum3").addEventListener("click", clickDrum3);
+document.getElementById("drum4").addEventListener("click", clickDrum4);
+document.getElementById("drum5").addEventListener("click", clickDrum5);
+document.getElementById("drum6").addEventListener("click", clickDrum6);
+document.getElementById("drum7").addEventListener("click", clickDrum7);
+document.getElementById("drum8").addEventListener("click", clickDrum8);
+document.getElementById("drum9").addEventListener("click", clickDrum9);
 function clickPlay() {
     const element = document.getElementById("loop-button");
     if (element.className == "loop-start-button") {
@@ -12,6 +19,39 @@ function clickPlay() {
     }
 };
 
-function clickDrum() {
-    console.log("drum");
+function clickDrum1() {
+    console.log("drum1");
+    document.getElementById("bassdrum").play();
+}
+function clickDrum2() {
+    console.log("drum2");
+    document.getElementById("snaredrum").play();
+}
+function clickDrum3() {
+    console.log("drum3");
+    document.getElementById("hihat").play();
+}
+function clickDrum4() {
+    console.log("drum4");
+    document.getElementById("tom1").play();
+}
+function clickDrum5() {
+    console.log("drum5");
+    document.getElementById("tom2").play();
+}
+function clickDrum6() {
+    console.log("drum6");
+    document.getElementById("tom3").play();
+}
+function clickDrum7() {
+    console.log("drum7");
+    document.getElementById("hihat-open").play();
+}
+function clickDrum8() {
+    console.log("drum8");
+    document.getElementById("ridecymbal").play();
+}
+function clickDrum9() {
+    console.log("drum9");
+    document.getElementById("crashcymbal").play();
 }
