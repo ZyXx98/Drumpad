318757ed1c0edde4d49fab298658b7d680258e2f
Mattis <mattis@petroll.de>
2022-04-27 11:01:18 +0200

    a

--- script/script.js
+++ script/script.js
@@ -1,11 +1,17 @@
 document.getElementById("loop-button").addEventListener("click", clickPlay);
+document.getElementsByClassName("drum").addEventListener("click", clickDrum);
 
 function clickPlay() {
     const element = document.getElementById("loop-button");
-    console.log(element);
     if (element.className == "loop-start-button") {
         element.className = "loop-pause-button";
+        element.innerHTML = "Pause";
     } else {
         element.className = "loop-start-button";
+        element.innerHTML = "Play";
     }
 };
+
+function clickDrum() {
+    console.log("drum");
+}
