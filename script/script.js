90aed414ba4c2b8ba1b3d0dca421b8512a068687
Mattis <mattis@petroll.de>
2022-05-07 15:04:06 +0200

    a

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 document.getElementById("loop-button").addEventListener("click", clickPlay);
+document.body.addEventListener("keydown", clickDrum);
 document.getElementById("drum1").addEventListener("click", clickDrum1);
 document.getElementById("drum2").addEventListener("click", clickDrum2);
 document.getElementById("drum3").addEventListener("click", clickDrum3);
@@ -8,6 +9,7 @@ document.getElementById("drum6").addEventListener("click", clickDrum6);
 document.getElementById("drum7").addEventListener("click", clickDrum7);
 document.getElementById("drum8").addEventListener("click", clickDrum8);
 document.getElementById("drum9").addEventListener("click", clickDrum9);
+document.getElementById("dropdownbutton").addEventListener("click", changeSound);
 function clickPlay() {
     const element = document.getElementById("loop-button");
     if (element.className == "loop-start-button") {
@@ -54,4 +56,43 @@ function clickDrum8() {
 function clickDrum9() {
     console.log("drum9");
     document.getElementById("crashcymbal").play();
+}
+function clickDrum(event) {
+    if (event.which == 49) {
+        console.log("drum1");
+        document.getElementById("bassdrum").play();
+    } else if (event.which == 50) {
+        console.log("drum2");
+        document.getElementById("snaredrum").play();
+    } else if (event.which == 51) {
+        console.log("drum3");
+        document.getElementById("hihat").play();
+    } else if (event.which == 52) {
+        console.log("drum4");
+        document.getElementById("tom1").play();
+    } else if (event.which == 53) {
+        console.log("drum5");
+        document.getElementById("tom2").play();
+    } else if (event.which == 54) {
+        console.log("drum6");
+        document.getElementById("tom3").play();
+    } else if (event.which == 55) {
+        console.log("drum7");
+        document.getElementById("hihat-open").play();
+    } else if (event.which == 56) {
+        console.log("drum8");
+        document.getElementById("ridecymbal").play();
+    } else if (event.which == 57) {
+        console.log("drum9");
+        document.getElementById("crashcymbal").play();
+    }
+}
+
+function changeSound() {
+    const element = document.getElementById("changeBassdrum");
+    if (element.className == "bassdrums-hidden") {
+        element.className = "bassdrums-shown";
+    } else {
+        element.className = "bassdrums-hidden";
+    }
 }
