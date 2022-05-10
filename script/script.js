f2e25c805e162091c77fd981311e63fc84b0307d
Mattis <mattis@petroll.de>
2022-05-10 13:32:57 +0200

    a

--- script/script.js
+++ script/script.js
@@ -9,7 +9,15 @@ document.getElementById("drum6").addEventListener("click", clickDrum6);
 document.getElementById("drum7").addEventListener("click", clickDrum7);
 document.getElementById("drum8").addEventListener("click", clickDrum8);
 document.getElementById("drum9").addEventListener("click", clickDrum9);
-document.getElementById("dropdownbutton").addEventListener("click", changeSound);
+document.getElementById("dropdownbutton1").addEventListener("click", changeSound);
+document.getElementById("dropdownbutton2").addEventListener("click", changeSound2);
+document.getElementById("dropdownbutton3").addEventListener("click", changeSound3);
+document.getElementById("dropdownbutton4").addEventListener("click", changeSound4);
+document.getElementById("dropdownbutton5").addEventListener("click", changeSound5);
+document.getElementById("dropdownbutton6").addEventListener("click", changeSound6);
+document.getElementById("dropdownbutton7").addEventListener("click", changeSound7);
+document.getElementById("dropdownbutton8").addEventListener("click", changeSound8);
+document.getElementById("dropdownbutton9").addEventListener("click", changeSound9);
 function clickPlay() {
     const element = document.getElementById("loop-button");
     if (element.className == "loop-start-button") {
@@ -88,11 +96,157 @@ function clickDrum(event) {
     }
 }
 
-function changeSound() {
+function changeSound1() {
     const element = document.getElementById("changeBassdrum");
-    if (element.className == "bassdrums-hidden") {
-        element.className = "bassdrums-shown";
+    if (element.className == "hidden") {
+        element.className = "shown";
+        document.getElementById("changeSnaredrum").className = "hidden";
+        document.getElementById("changeHiHat").className = "hidden";
+        document.getElementById("changeTom1").className = "hidden";
+        document.getElementById("changeTom2").className = "hidden";
+        document.getElementById("changeTom3").className = "hidden";
+        document.getElementById("changeHiHat-open").className = "hidden";
+        document.getElementById("changeRidecymbal").className = "hidden";
+        document.getElementById("changeCrashcymbal").className = "hidden";
     } else {
-        element.className = "bassdrums-hidden";
+        element.className = "hidden";
+    }
+}
+function changeSound2() {
+    const element = document.getElementById("changeSnaredrum");
+    if (element.className == "hidden") {
+        element.className = "shown";
+        document.getElementById("changeBassdrum").className = "hidden";
+        document.getElementById("changeHiHat").className = "hidden";
+        document.getElementById("changeTom1").className = "hidden";
+        document.getElementById("changeTom2").className = "hidden";
+        document.getElementById("changeTom3").className = "hidden";
+        document.getElementById("changeHiHat-open").className = "hidden";
+        document.getElementById("changeRidecymbal").className = "hidden";
+        document.getElementById("changeCrashcymbal").className = "hidden";
+    } else {
+        element.className = "hidden";
+    }
+}
+function changeSound3() {
+    const element = document.getElementById("changeHiHat");
+    if (element.className == "hidden") {
+        element.className = "shown";
+        document.getElementById("changeBassdrum").className = "hidden";
+        document.getElementById("changeSnaredrum").className = "hidden";
+        document.getElementById("changeTom1").className = "hidden";
+        document.getElementById("changeTom2").className = "hidden";
+        document.getElementById("changeTom3").className = "hidden";
+        document.getElementById("changeHiHat-open").className = "hidden";
+        document.getElementById("changeRidecymbal").className = "hidden";
+        document.getElementById("changeCrashcymbal").className = "hidden";
+    } else {
+        element.className = "hidden";
+    }
+}
+function changeSound4() {
+    const element = document.getElementById("changeTom1");
+    if (element.className == "hidden") {
+        element.className = "shown";
+        document.getElementById("changeBassdrum").className = "hidden";
+        document.getElementById("changeSnaredrum").className = "hidden";
+        document.getElementById("changeHiHat").className = "hidden";
+        document.getElementById("changeTom2").className = "hidden";
+        document.getElementById("changeTom3").className = "hidden";
+        document.getElementById("changeHiHat-open").className = "hidden";
+        document.getElementById("changeRidecymbal").className = "hidden";
+        document.getElementById("changeCrashcymbal").className = "hidden";
+    } else {
+        element.className = "hidden";
+    }
+}
+function changeSound5() {
+    const element = document.getElementById("changeTom2");
+    if (element.className == "hidden") {
+        element.className = "shown";
+        document.getElementById("changeBassdrum").className = "hidden";
+        document.getElementById("changeSnaredrum").className = "hidden";
+        document.getElementById("changeHiHat").className = "hidden";
+        document.getElementById("changeTom1").className = "hidden";
+        document.getElementById("changeTom3").className = "hidden";
+        document.getElementById("changeHiHat-open").className = "hidden";
+        document.getElementById("changeRidecymbal").className = "hidden";
+        document.getElementById("changeCrashcymbal").className = "hidden";
+    } else {
+        element.className = "hidden";
+    }
+}
+function changeSound6() {
+    const element = document.getElementById("changeTom3");
+    if (element.className == "hidden") {
+        element.className = "shown";
+        document.getElementById("changeBassdrum").className = "hidden";
+        document.getElementById("changeSnaredrum").className = "hidden";
+        document.getElementById("changeHiHat").className = "hidden";
+        document.getElementById("changeTom1").className = "hidden";
+        document.getElementById("changeTom2").className = "hidden";
+        document.getElementById("changeHiHat-open").className = "hidden";
+        document.getElementById("changeRidecymbal").className = "hidden";
+        document.getElementById("changeCrashcymbal").className = "hidden";
+    } else {
+        element.className = "hidden";
+    }
+}
+function changeSound7() {
+    const element = document.getElementById("changeHiHat-open");
+    if (element.className == "hidden") {
+        element.className = "shown";
+        document.getElementById("changeBassdrum").className = "hidden";
+        document.getElementById("changeSnaredrum").className = "hidden";
+        document.getElementById("changeHiHat").className = "hidden";
+        document.getElementById("changeTom1").className = "hidden";
+        document.getElementById("changeTom2").className = "hidden";
+        document.getElementById("changeTom3").className = "hidden";
+        document.getElementById("changeRidecymbal").className = "hidden";
+        document.getElementById("changeCrashcymbal").className = "hidden";
+    } else {
+        element.className = "hidden";
+    }
+}
+function changeSound8() {
+    const element = document.getElementById("changeRidecymbal");
+    if (element.className == "hidden") {
+        element.className = "shown";
+        document.getElementById("changeBassdrum").className = "hidden";
+        document.getElementById("changeSnaredrum").className = "hidden";
+        document.getElementById("changeHiHat").className = "hidden";
+        document.getElementById("changeTom1").className = "hidden";
+        document.getElementById("changeTom2").className = "hidden";
+        document.getElementById("changeTom3").className = "hidden";
+        document.getElementById("changeHiHat-open").className = "hidden";
+        document.getElementById("changeCrashcymbal").className = "hidden";
+    } else {
+        element.className = "hidden";
+    }
+}
+function changeSound9() {
+    const element = document.getElementById("changeCrashcymbal");
+    if (element.className == "hidden") {
+        element.className = "shown";
+        document.getElementById("changeBassdrum").className = "hidden";
+        document.getElementById("changeSnaredrum").className = "hidden";
+        document.getElementById("changeHiHat").className = "hidden";
+        document.getElementById("changeTom1").className = "hidden";
+        document.getElementById("changeTom2").className = "hidden";
+        document.getElementById("changeTom3").className = "hidden";
+        document.getElementById("changeHiHat-open").className = "hidden";
+        document.getElementById("changeRidecymbal").className = "hidden";
+    } else {
+        element.className = "hidden";
+    }
+}
+
+function changeSound() {
+    const elementList = document.querySelectorAll("div.query");
+    console.log(elementList);
+    if () {
+
+    } else {
+
     }
 }
